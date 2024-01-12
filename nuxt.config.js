6d333a9e7f0bbdeeb15f65adc5c85359268ea258
miraclemichael018 <michaelmirade018@gmail.com>
2024-01-12 17:04:52 +0000

    some chnages made

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,43 @@
 module.exports = {
+  ssr: true,
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'podcast-dashboard',
+    title: "podcast-dashboard",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: { color: "#3B8070" },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   css: ["@/assets/css/main.css"],
-  buildModules: [
-    '@nuxtjs/vuetify', 'nuxt-gsap-module'
-  ],
-  mode: 'spa',
+  buildModules: ["@nuxtjs/vuetify", "nuxt-gsap-module"],
+  mode: "spa",
   router: {
-    base: '/podcast-dashboard/'
+    base: "/podcast-dashboard/"
   },
   build: {
     /*
-    ** Run ESLint on save
-    */
+     ** Run ESLint on save
+     */
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
